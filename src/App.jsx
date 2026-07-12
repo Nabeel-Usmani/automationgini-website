0db516eeabb93086de0cb0b8081adc92f16749b5
Nabeel Usmani <130184043+Nabeel-Usmani@users.noreply.github.com>
2026-07-12 21:44:36 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Auth from './pages/Auth'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Auth initialMode="signup" />} />
+        <Route path="/login" element={<Auth initialMode="signin" />} />
       </Routes>
     </BrowserRouter>
   )
