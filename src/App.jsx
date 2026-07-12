c9d1643e97950b0a8b49e4ba570a5f48a4c124ac
Nabeel Usmani <130184043+Nabeel-Usmani@users.noreply.github.com>
2026-07-12 22:36:52 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 
+const GOOGLE_CLIENT_ID = 'REPLACE_WITH_GOOGLE_CLIENT_ID'
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Auth initialMode="signup" />} />
-        <Route path="/login" element={<Auth initialMode="signin" />} />
-      </Routes>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Auth initialMode="signup" />} />
+          <Route path="/login" element={<Auth initialMode="signin" />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   )
 }
