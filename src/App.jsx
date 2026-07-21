70557a62ba4eb3b63392df0a385650baa787e671
Nabeel Usmani <130184043+Nabeel-Usmani@users.noreply.github.com>
2026-07-21 12:46:03 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import ChatWidget from './components/ChatWidget'
 
 const GOOGLE_CLIENT_ID = '31181968054-ta74c5q9baeqqkrb636g7iauoa612f2c.apps.googleusercontent.com'
 
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/signup" element={<Auth initialMode="signup" />} />
           <Route path="/login" element={<Auth initialMode="signin" />} />
         </Routes>
+        <ChatWidget />
       </BrowserRouter>
     </GoogleOAuthProvider>
   )
